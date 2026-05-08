8d0aa83b500c136b85ca0ac69d30001bb9e9cd62
Nikita Tkachuk <nnikkito019@gmail.com>
2026-05-08 14:37:56 +0300

    tested different variants of content loading

--- web/modules/my_first_module/src/Controller/MyPageController.php
+++ web/modules/my_first_module/src/Controller/MyPageController.php
@@ -6,10 +6,59 @@ use Drupal\Core\Controller\ControllerBase;
 
 class MyPageController extends ControllerBase
 {
+    // use of MARKUP
+    // public function content(): array
+    // {
+    //     return [
+    //         '#markup' => 'Hello, Drupal!<br>' . date('d.m.Y H:i'),
+    //         '#cache' => [
+    //             'max-age' => 0,
+    //         ],
+    //     ];
+    // }
+
+    // use of HTML WITH MARKUP
+    // public function content(): array
+    // {
+    //     return [
+    //         '#markup' => '
+    //     <div class="my-page">
+    //         <h5>Hello, Drupal!</h5>
+    //         <p>' . date('d.m.Y H:i') . '</p>
+    //     </div>
+    // ',
+    //         '#cache' => [
+    //             'max-age' => 0,
+    //         ],
+    //     ];
+    // }
+
+    // use of HTML RENDER ARRAY
+    // public function content(): array
+    // {
+    //     return [
+    //         'title' => [
+    //             '#type' => 'html_tag',
+    //             '#tag' => 'h5',
+    //             '#value' => 'Hello, Drupal!',
+    //         ],
+    //         'date' => [
+    //             '#type' => 'html_tag',
+    //             '#tag' => 'p',
+    //             '#value' => date('d.m.Y H:i'),
+    //         ],
+    //         '#cache' => [
+    //             'max-age' => 0,
+    //         ],
+    //     ];
+    // }
+
+    // use of TWIG TEMPLATE
     public function content(): array
     {
         return [
-            '#markup' => 'Hello, Drupal!<br>' . date('d.m.Y H:i'),
+            '#theme' => 'my_first_page',
+            '#date' => date('d.m.Y H:i'),
             '#cache' => [
                 'max-age' => 0,
             ],
